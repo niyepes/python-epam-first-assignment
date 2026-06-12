0af1010136ea82651246660f2ee882aa8ed8bda4
Nicolás Yepes <nayepesc@udistrital.edu.co>
2026-06-12 09:39:48 -0500

    fix:Segundo intento de arreglar los tests; se aregó el if __name__ == __main__ en el ejercicio de n_letter

--- n_letter.py
+++ n_letter.py
@@ -1,6 +1,7 @@
 def nth_char(words):
     return ''.join(word[n] for n, word in enumerate(words))
  
- 
-words = input("Enter words separated by spaces: ").split()
-print(nth_char(words))
+if __name__ == "__main__":
+    words = input("Enter words separated by spaces: ").split()
+    print(nth_char(words))
+
